9a30f50b144dd5eacfa4dc8188816b46503a66ce
itgsod-anton-skogsberg <anton.skogsberg@elev.it-gymnasiet.se>
2014-04-29 11:40:39 +0200

    Börjat encryptering av cleartext bokstävernas position

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,3 +1,8 @@
-def encrypt(x, y):
-    if len(x) == 0:
+def encrypt(cleartext, offset):
+    if len(cleartext) == 0:
         raise ValueError('can not encrypt empty string')
+    if offset == 0:
+        raise ValueError('offset must not be zero')
+    alphabetic_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].index(cleartext[i])
+    for x in alphabetic_letters
+        return cleartext.upper()tters:
