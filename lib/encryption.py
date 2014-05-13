a607ef1aea0f00e658a30e275de37ec042fc1b65
itgsod-anton-skogsberg <anton.skogsberg@elev.it-gymnasiet.se>
2014-05-13 09:51:07 +0200

    Flowchart
    Added flowchart for encryption

--- lib/encryption.py
+++ lib/encryption.py
@@ -5,4 +5,4 @@ def encrypt(cleartext, offset):
         raise ValueError('offset must not be zero')
     alphabetic_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].index(cleartext[i])
     for x in alphabetic_letters
-        return cleartext.upper()tters:
+        return cleartext.upper():
