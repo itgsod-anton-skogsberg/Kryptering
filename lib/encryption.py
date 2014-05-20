69edefef5fc0a1815fbff7d4a7a7148cce207737
itgsod-anton-skogsberg <anton.skogsberg@elev.it-gymnasiet.se>
2014-05-20 11:33:55 +0200

    Decryption
    Decryption done

--- lib/encryption.py
+++ lib/encryption.py
@@ -17,3 +17,6 @@ def encrypt(cleartext, offset):
             new_pos = alphabet.index(char) + offset
             output += alphabet[new_pos]
     return output.upper()
+
+def encrypt(cleartext, offset):
+    return encrypt(cleartext, offset).lower()
