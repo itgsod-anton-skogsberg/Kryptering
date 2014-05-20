1228994b3df9303a7ec53ac339e24c6f2624b496
itgsod-anton-skogsberg <anton.skogsberg@elev.it-gymnasiet.se>
2014-05-20 11:17:26 +0200

    Encryption done
    All strings are returned correctly

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,8 +1,19 @@
 def encrypt(cleartext, offset):
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                          't', 'u', 'v', 'w', 'x', 'y', 'z']
     if len(cleartext) == 0:
         raise ValueError('can not encrypt empty string')
     if offset == 0:
         raise ValueError('offset must not be zero')
-    alphabetic_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'].index(cleartext[i])
-    for x in alphabetic_letters
-        return cleartext.upper():
+    output = ''
+    for char in cleartext:
+        char = char.lower()
+        if char == ' ':
+            output += ' '
+        elif alphabet.index(char) + offset > 25:
+            new_pos =+ alphabet.index(char) + offset - 26
+            output += alphabet[new_pos]
+        else:
+            new_pos = alphabet.index(char) + offset
+            output += alphabet[new_pos]
+    return output.upper()
